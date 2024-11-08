0191c9b8789b84d3783a4487b1f6b19116f3fd2d
om <omsatodiya96@gmail.com>
2024-11-08 07:16:53 +0530

    Bit Manipulation Practice Problems

--- Difficulty 1000/B_Fedor_and_New_Game.cpp
+++ Difficulty 1000/B_Fedor_and_New_Game.cpp
@@ -16,55 +16,79 @@ using namespace std;
 #define ll long long int
 #define all(p) p.begin(), p.end()
 #define um unordered_map
-#define ok(x)              \
-    {                      \
-        cout << x << endl; \
-        return;            \
-    }
+#define ok(x)          \
+  {                    \
+    cout << x << endl; \
+    return;            \
+  }
 
 // Bit Manipulation
 
 bool isPowerOfTwo(unsigned int n)
 {
-    return n && !(n & (n - 1));
+  return n && !(n & (n - 1));
 }
 
 bool isDivisibleByPowerOf2(int n, int k)
 {
-    int powerOf2 = 1 << k;
-    return (n & (powerOf2 - 1)) == 0;
+  int powerOf2 = 1 << k;
+  return (n & (powerOf2 - 1)) == 0;
 }
 
 int countSetBits(int n)
 {
-    int count = 0;
-    while (n)
-    {
-        n = n & (n - 1);
-        count++;
-    }
-    return count;
+  int count = 0;
+  while (n)
+  {
+    n = n & (n - 1);
+    count++;
+  }
+  return count;
 }
 
 /* ------------------------------------------------- TEMPLATE ENDS --------------------------------------------------*/
 
 void solve()
 {
+  int n, m, k;
+  cin >> n >> m >> k;
+
+  int players[m + 1];
+  for (int i = 0; i <= m; i++)
+  {
+    cin >> players[i];
+  }
+
+  int fedor = players[m];
+  int cnt = 0;
+
+  for (int i = 0; i < m; i++)
+  {
+    int hammingDistance = countSetBits(players[i] ^ fedor);
+    if (hammingDistance <= k)
+    {
+      cnt++;
+    }
+  }
+
+  cout << cnt << endl;
 }
 
 int main()
 {
 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int t;
-    cin >> t;
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  // int t;
+  // cin >> t;
 
-    while (t--)
-    {
-        solve();
-    }
+  // while (t--)
+  // {
+  //   solve();
+  // }
 
-    return 0;
+  solve();
+
+  return 0;
 }
--- Difficulty 800/A_And_Then_There_Were_K.cpp
+++ Difficulty 800/A_And_Then_There_Were_K.cpp
@@ -16,55 +16,66 @@ using namespace std;
 #define ll long long int
 #define all(p) p.begin(), p.end()
 #define um unordered_map
-#define ok(x)              \
-    {                      \
-        cout << x << endl; \
-        return;            \
-    }
+#define ok(x)          \
+  {                    \
+    cout << x << endl; \
+    return;            \
+  }
 
 // Bit Manipulation
 
 bool isPowerOfTwo(unsigned int n)
 {
-    return n && !(n & (n - 1));
+  return n && !(n & (n - 1));
 }
 
 bool isDivisibleByPowerOf2(int n, int k)
 {
-    int powerOf2 = 1 << k;
-    return (n & (powerOf2 - 1)) == 0;
+  int powerOf2 = 1 << k;
+  return (n & (powerOf2 - 1)) == 0;
 }
 
 int countSetBits(int n)
 {
-    int count = 0;
-    while (n)
-    {
-        n = n & (n - 1);
-        count++;
-    }
-    return count;
+  int count = 0;
+  while (n)
+  {
+    n = n & (n - 1);
+    count++;
+  }
+  return count;
 }
 
 /* ------------------------------------------------- TEMPLATE ENDS --------------------------------------------------*/
 
 void solve()
 {
+  int n;
+  cin >> n;
+  int length = 0;
+  while (n > 0)
+  {
+    n >>= 1;
+    length++;
+  }
+
+  long long ans = pow(2, length - 1) - 1;
+  cout << ans << endl;
 }
 
 int main()
 {
 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int t;
-    cin >> t;
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int t;
+  cin >> t;
 
-    while (t--)
-    {
-        solve();
-    }
+  while (t--)
+  {
+    solve();
+  }
 
-    return 0;
+  return 0;
 }
